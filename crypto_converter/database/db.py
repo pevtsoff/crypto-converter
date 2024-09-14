187f385f537801a89320e2e892a1029589831ca9
Ivan Pevtsov <ivan.pevtsov@gmail.com>
2024-09-14 13:19:44 +0300

    drafted conftest.py

--- crypto_converter/database/db.py
+++ crypto_converter/database/db.py
@@ -3,7 +3,7 @@ from typing import Any, AsyncIterator, AsyncGenerator
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
-    create_async_engine,
+    create_async_engine, AsyncConnection,
 )
 from sqlalchemy.orm import declarative_base
 from crypto_converter.common.common import configure_logger
@@ -31,6 +31,11 @@ async def get_db_session() -> AsyncSession:
     finally:
         await db.close()
 
+@asynccontextmanager
+async def get_db_connection() -> AsyncConnection:
+    """Factory that returns an async connection."""
+    async with engine.connect() as connection:
+        yield connection
 
 # This option works alone fine without using transaction context manager below
 # async def get_db_session() -> AsyncSession:
---
+++ crypto_converter/tests/test_models.py
@@ -0,0 +1,13 @@
+import pytest
+
+from crypto_converter.database.db_models import BinanceTickerModel
+
+@pytest.mark.asyncio
+async def test_model_creation(setup_database,db_session):
+    session = await anext(db_session)
+    ticker = BinanceTickerModel(
+        ticker_name="btsusdt",
+        price="1232134242",
+        timestamp=2354646546,
+    )
+    session.add(ticker)
