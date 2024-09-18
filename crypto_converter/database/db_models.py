b13350749cc7812f80bdb07b3c8fcf1323757c80
Ivan Pevtsov <ivan.pevtsov@gmail.com>
2024-09-18 16:40:34 +0400

    added new ticker_aggregated_data_model

--- crypto_converter/database/db_models.py
+++ crypto_converter/database/db_models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from typing import List
-from sqlalchemy import BigInteger, String, JSON, ForeignKey
+from sqlalchemy import BigInteger, String, JSON, ForeignKey, Float
 from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship
 import sqlalchemy as sa
 
@@ -25,6 +25,17 @@ class BinanceTickerDataModel(Base):
     json_data = mapped_column(JSON)
 
 
+class BinanceTickerAggregatedData(Base):
+    __tablename__ = "binance_tickers_aggregated_data"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    ticker: Mapped["BinanceTickersModel"] = relationship()
+
+    min_price: Mapped[float] = mapped_column(Float(50))
+    avg_price: Mapped[float] = mapped_column(Float(50))
+    max_price: Mapped[float] = mapped_column(Float(50))
+
+
 class BinanceTickersModel(Base):
     __tablename__ = "binance_tickers_list"
 
@@ -32,5 +43,8 @@ class BinanceTickersModel(Base):
     ticker_name: Mapped[str] = mapped_column(String(50), index=True)
     ticker_data: Mapped[List[BinanceTickerDataModel]] = relationship(
         # cascade="all, delete-orphan",
+        passive_deletes="all"
+    )
+    ticker_aggregated_data: Mapped[List[BinanceTickerAggregatedData]] = relationship(
         passive_deletes="all"
     )
