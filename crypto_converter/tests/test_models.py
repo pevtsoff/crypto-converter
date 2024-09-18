503d6d31b5e032b2eb578ea39da35b1f1741d735
Ivan Pevtsov <ivan.pevtsov@gmail.com>
2024-09-18 17:11:24 +0400

    removed the wrong migrations

--- crypto_converter/tests/test_models.py
+++ crypto_converter/tests/test_models.py
@@ -1,13 +1,18 @@
 import pytest
-from crypto_converter.database.db_models import BinanceTickerModel
+from crypto_converter.database.db_models import (
+    BinanceTickersModel,
+    BinanceTickerDataModel,
+)
 
 
 @pytest.mark.asyncio
 async def test_model_creation(db_session):
     session = db_session
-    ticker = BinanceTickerModel(
+    ticker = BinanceTickersModel(
         ticker_name="btsusdt",
-        price="1232134242",
-        timestamp=2354646546,
     )
+    ticker_data = BinanceTickerDataModel(
+        price="1232134242", timestamp=2354646546, json_data="{}"
+    )
+    ticker.ticker_data.append(ticker_data)
     session.add(ticker)
