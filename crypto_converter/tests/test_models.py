c226b6b5fea023ce36eab086b0d3928908aaa262
Ivan Pevtsov <ivan.pevtsov@gmail.com>
2024-09-23 09:22:20 +0400

    finished the models test

--- crypto_converter/tests/test_models.py
+++ crypto_converter/tests/test_models.py
@@ -9,19 +9,29 @@ from sqlalchemy import select
 @pytest.mark.asyncio
 async def test_model_creation(db_engine, db_session_2, event_loop):
     session = db_session_2
+    # test data
+    test_ticker_name = "btcusdt"
+    test_price = "1232134242"
+    test_timestamp = 2354646546
+
     ticker = BinanceTickersModel(
-        ticker_name="btcusdt",
+        ticker_name=test_ticker_name,
     )
     ticker_data = BinanceTickerDataModel(
-        price="1232134242", timestamp=2354646546, json_data="{}"
+        price=test_price, timestamp=test_timestamp, json_data="{}"
     )
+
     ticker.ticker_data.append(ticker_data)
     session.add(ticker)
-    session.add(ticker_data)
+
     await session.commit()
+
+    # Getting saved actual ticker and its data
     stmt = select(BinanceTickersModel).where(
-        BinanceTickersModel.ticker_name == "btcusdt"
+        BinanceTickersModel.ticker_name == test_ticker_name,
     )
     result = await session.execute(stmt)
-    t = result.scalars().first()
-    assert t is not None
+    actual_ticker = result.scalars().first()
+
+    assert actual_ticker.id is not None
+    assert actual_ticker.ticker_data[0] == ticker_data
