2710889e775d14641080288499bad67bc1bd4f27
Ivan Pevtsov <ivan.pevtsov@gmail.com>
2024-09-23 10:38:17 +0400

    added DEV_MODE env var

--- crypto_converter/api/app.py
+++ crypto_converter/api/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.encoders import decimal_encoder
 
 from crypto_converter.api.aggregation.aggregation_api import aggregation_router
+from crypto_converter.common import settings
 from crypto_converter.common.exception_handlers import common_exception_handler
 import uvicorn
 
@@ -24,7 +25,13 @@ def create_fastapi_app():
 def start_exchange_api():
     app = create_fastapi_app()
     app.json_encoder = decimal_encoder
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        reload=settings.DEV_MODE,
+        workers=settings.UVICORN_WORKERS,
+    )
 
 
 if __name__ == "__main__":
