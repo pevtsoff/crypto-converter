f61a6cba240b1c6085d73cf180d77d80ef3eb0f0
Ivan Pevtsov <ivan.pevtsov@gmail.com>
2024-09-23 10:30:45 +0400

    fixed event_loop issue, using embedded event_loop in pytest-asyncio fixture

--- crypto_converter/tests/conftest.py
+++ crypto_converter/tests/conftest.py
@@ -104,14 +104,6 @@ def run_migrations(connection: Connection):
             context.run_migrations()
 
 
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     """Create an event loop for the test session."""
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
-
-
 @pytest.fixture
 async def db_engine(create_test_database):
     engine = create_async_engine(
